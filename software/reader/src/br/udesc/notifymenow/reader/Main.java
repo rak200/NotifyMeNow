b2a84271fa392ced014f5c9c0039a57aa8f63518
Ricardo Augusto Kustner <rak.ricardo@windowslive.com>
2017-05-15 21:37:21 -0300

    #6 - testes com SQLite

--- software/reader/src/br/udesc/notifymenow/reader/Main.java
+++ software/reader/src/br/udesc/notifymenow/reader/Main.java
@@ -9,7 +9,11 @@ import br.udesc.notifymenow.reader.controller.rss.RssReaderFactory;
 import br.udesc.notifymenow.reader.controller.rss.RssReader;
 import br.udesc.notifymenow.reader.model.Noticia;
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 /**
@@ -28,6 +32,50 @@ public class Main {
             System.out.println(feed.getConteudo());
             System.out.println(feed.getLink());
             System.out.println(String.format("\n"));
+        }
+
+        teste();
+    }
+
+    private static teste() {
+        Connection connection = null;
+        try
+        {
+            // create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite:db/nmn.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            statement.executeUpdate("drop table if exists person");
+            statement.executeUpdate("create table person (id integer, name string)");
+            statement.executeUpdate("insert into person values(1, 'leo')");
+            statement.executeUpdate("insert into person values(2, 'yui')");
+            ResultSet rs = statement.executeQuery("select * from person");
+            while(rs.next())
+            {
+                // read the result set
+                System.out.println("name = " + rs.getString("name"));
+                System.out.println("id = " + rs.getInt("id"));
+            }
+          }
+          catch(SQLException e)
+          {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+          }
+          finally
+          {
+            try
+            {
+              if(connection != null)
+                connection.close();
+            }
+            catch(SQLException e)
+            {
+              // connection close failed.
+              System.err.println(e);
+            }
         }
     }
 
